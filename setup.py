03fcb48c2416a2018de75cd4f5370877bf06d605
Bence Nagy <bence@underyx.me>
2016-08-11 00:18:22 +0200

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with io.open('README.md', encoding='utf-8') as f:
 
 setup(
     name='zenbot',
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/underyx/zenbot',
     author='Bence Nagy',
     author_email='bence@underyx.me',
